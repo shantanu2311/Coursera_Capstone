c6eb98014620bc69a9402a942a23cc3147313f50
shantanu2311 <64919534+shantanu2311@users.noreply.github.com>
2020-07-06 07:59:21 +0530

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # This notebook will be used for Capstone project
+
+# In[4]:
+
+
+import pandas as pd
+import numpy as np
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
